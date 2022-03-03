07b8545744765cd75b6f18b5d5d8a451f50030b1
Vinod MultiSafepay <vinod.sowdagar@multisafepay.com>
2022-03-03 17:48:46 +0100

    Change letters to numbers for consistency

--- Override/SalesSequence/ResourceModel/Meta.php
+++ Override/SalesSequence/ResourceModel/Meta.php
@@ -4,39 +4,30 @@ declare(strict_types=1);
 namespace Vino\RandomOrderPrefix\Override\SalesSequence\ResourceModel;
 
 
+use Exception;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Model\AbstractModel;
 use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
 use Magento\Framework\Model\ResourceModel\Db\Context as DatabaseContext;
 use Magento\SalesSequence\Model\MetaFactory;
 use Magento\SalesSequence\Model\ResourceModel\Profile as ResourceProfile;
-use Vino\RandomOrderPrefix\Model\Prefix;
 
 class Meta extends \Magento\SalesSequence\Model\ResourceModel\Meta
 {
-    /**
-     * @var Prefix
-     */
-    private $prefix;
-
     /**
      * Meta constructor.
      *
      * @param DatabaseContext $context
      * @param MetaFactory $metaFactory
      * @param ResourceProfile $resourceProfile
-     * @param Prefix $prefix
      * @param null $connectionName
      */
     public function __construct(
         DatabaseContext $context,
         MetaFactory $metaFactory,
         ResourceProfile $resourceProfile,
-        Prefix $prefix,
         $connectionName = null
     ) {
-        $this->prefix = $prefix;
-
         parent::__construct($context, $metaFactory, $resourceProfile, $connectionName);
     }
 
@@ -44,13 +35,14 @@ class Meta extends \Magento\SalesSequence\Model\ResourceModel\Meta
      * @param AbstractModel $object
      * @return $this|AbstractDb|\Magento\SalesSequence\Model\ResourceModel\Meta
      * @throws LocalizedException
+     * @throws Exception
      */
     protected function _afterLoad(AbstractModel $object)
     {
-        $randomPrefix = $this->prefix->generateRandom();
+        $randomPrefix = $this->generateRandom();
 
         $activeProfile = $this->resourceProfile->loadActiveProfile($object->getId());
-        $activeProfile->setPrefix($randomPrefix . '-');
+        $activeProfile->setPrefix($randomPrefix);
 
         $object->setData(
             'active_profile',
@@ -58,6 +50,15 @@ class Meta extends \Magento\SalesSequence\Model\ResourceModel\Meta
         );
 
         return $this;
+    }
+
+    /**
+     * @return int
+     * @throws Exception
+     */
+    public function generateRandom(): int
+    {
+        return random_int(0, 999999);
     }
 
 }
